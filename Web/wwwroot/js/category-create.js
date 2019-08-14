995a4cfc751fdda1f1844462ad79b60f9b22c6e0
Božo Čulo <bculo@foi.hr>
2019-08-14 19:50:08 +0200

    js refactoring

---
+++ Web/wwwroot/js/category-create.js
@@ -0,0 +1,38 @@
+﻿var my = {};
+
+my.viewname = {
+    init: function (settings) {
+        my.uploadUrl = settings.uploadUrl;
+        my.nameInputId = settings.nameInputId;
+        my.nameInputSelector = "#".concat(settings.nameInputId);
+        my.categoryImageInputId = settings.categoryImageInputId;
+        my.categoryImageSelector = "#".concat(settings.categoryImageInputId);
+    }
+}
+
+var upload = new FileUpload();
+
+window.onload = function () {
+    upload.setUploadUrl(my.uploadUrl);
+
+    let token = $('input[name="__RequestVerificationToken"]').val();
+    upload.addItem("__RequestVerificationToken", token);
+
+    $("#submitForm").click(submitFile);
+}
+
+function submitFile(e) {
+    e.preventDefault();
+
+    let file = $(my.categoryImageSelector).prop('files')[0];
+    let name = $(my.nameInputSelector).val();
+
+    if (!name || !file) {
+        console.log("Something aint riht");
+        return;
+    }
+
+    upload.addItem(my.nameInputId, name);
+    upload.addItem(my.categoryImageInputId, file);
+    upload.uploadForm();
+}
--- Web/wwwroot/js/file-upload.js
+++ Web/wwwroot/js/file-upload.js
@@ -5,18 +5,8 @@ function Item(key, value) {
 }
 
 function FileUpload() {
-    var antiforgeryToken = null;
-    var fileUpload;
-    var items = []; // array of FilePair items
-    var destinationUrl = ""; // upload destination
-
-    this.setFile = function (key, file) {
-        fileUpload = new Item(key, file);
-    }
-
-    this.setForgeryToken = function (key, file) {
-        antiforgeryToken = new Item(key, file);
-    }
+    var items = []; 
+    var destinationUrl = ""; 
 
     this.setUploadUrl = function (url) {
         destinationUrl = url;
@@ -30,19 +20,8 @@ function FileUpload() {
     this.uploadForm = function () {
         let formData = new FormData();
 
-        console.log(fileUpload);
-        console.log(antiforgeryToken);
-
         for (var item of items) {
             formData.append(item.key, item.value)
-        }
-
-        if (fileUpload != null) {
-            formData.append(fileUpload.key, fileUpload.value);
-        }
-
-        if (antiforgeryToken != null) {
-            formData.append(antiforgeryToken.key, antiforgeryToken.value);
         }
 
         $.ajax({
@@ -52,10 +31,10 @@ function FileUpload() {
             processData: false,
             data: formData,
             success: function (message) {
-                alert(message);
+                //Add return
             },
             error: function () {
-                alert("ERROR!");
+                //Add return
             }
         });
     }
